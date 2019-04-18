a94aeb4123112a435f2b788d87ef9b755238197a
zmichgen <zmichgen@gmail.com>
2019-04-18 11:40:04 +0300

    feat: add bones bounce

--- src/app/fishka/fishka.component.ts
+++ src/app/fishka/fishka.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class FishkaComponent implements OnInit {
   @Input() value: number;
   @Input() id: number;
+  @Input() emptyItemPos: number;
+  @Input() arrData: number[];
+  @Input() move;
   @Output() check = new EventEmitter();
   condition: boolean;
 
@@ -19,5 +22,42 @@ export class FishkaComponent implements OnInit {
 
   ngOnInit() {
     this.condition = (16 !== this.value);
+  }
+
+  toggle($event) {
+    const id = $event.target.id;
+    const elPos = this.arrData.indexOf(+id);
+    const ch = this.emptyItemPos - elPos;
+
+    switch (ch) {
+      case this.move.down:
+      case this.move.up:
+      case this.move.right:
+      case this.move.left: {
+
+        document.getElementById(id).classList.add('fishka-over');
+        break;
+      }
+      default: { break; }
+    }
+  }
+
+  untoggle($event) {
+    const id = $event.target.id;
+    const elPos = this.arrData.indexOf(+id);
+    const ch = this.emptyItemPos - elPos;
+
+    switch (ch) {
+      case this.move.down:
+      case this.move.up:
+      case this.move.right:
+      case this.move.left: {
+
+        document.getElementById(id).classList.remove('fishka-over');
+        break;
+      }
+      default: { break; }
+    }
+
   }
 }
